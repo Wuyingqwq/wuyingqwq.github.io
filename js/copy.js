d2f4bc825ccdb0e0286a8efa71955fef72c8ab0c
Wuyingqwq <88446363+Wuyingqwq@users.noreply.github.com>
2022-06-06 11:14:00 +0800

    Delete js directory

--- js/copy.js
+++
@@ -1,6 +0,0 @@
-function copyGithubLink(){
-    var Url2 = document.getElementById("Github");
-    Url2.select(); // 选择对象
-    document.execCommand("Copy"); // 执行浏览器复制命令
-}
-
--- js/reload.js
+++
@@ -1,3 +0,0 @@
-function reloadHtml(){
-    location.reload();
-}
