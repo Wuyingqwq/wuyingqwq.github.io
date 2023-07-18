9771009eba54fc3fbbf4216dbcf0ab4e196dc731
Wuyingqwq <88446363+Wuyingqwq@users.noreply.github.com>
2023-07-18 08:11:20 +0800

    Update main.js

--- hunxiao/main.js
+++ hunxiao/main.js
@@ -1,5 +1,25 @@
 function replace_jie(str) {
     switch (str) {
+        case "Q":
+            return '1'
+        case "W":
+            return '2'
+        case "E":
+            return '3'
+        case "R":
+            return '4'
+        case "T":
+            return '5'
+        case "Y":
+            return '6'
+        case "U":
+            return '7'
+        case "I":
+            return '8'
+        case "O":
+            return '9'
+        case "P":
+            return '0'
         case '1':
             return "Q"
         case '2':
@@ -68,11 +88,39 @@ function replace_jie(str) {
             return " "
         default:
             return "ERR"
+        case "+":
+            return "/"
+        case "=":
+            return ":"
+        case "[":
+            return "."
+        case ">":
+            return "?"
     }
 }
 
 function replace_jia(str) {
     switch (str) {
+        case '1':
+            return "Q"
+        case '2':
+            return "W"
+        case '3':
+            return "E"
+        case '4':
+            return "R"
+        case '5':
+            return "T"
+        case '6':
+            return "Y"
+        case '7':
+            return "U"
+        case '8':
+            return "I"
+        case '9':
+            return "O"
+        case '0':
+            return "P"
         case "Q":
             return '1'
         case "W":
@@ -127,11 +175,20 @@ function replace_jia(str) {
             return "、"
         case " ":
             return " "
+        case "/":
+            return "+"
+        case ":":
+            return "="
+        case ".":
+            return "["
+        case "?":
+            return ">"
         default:
             return "ERR"
     }
 }
 
+var ul = [];
 
 function jia() {
     var str = document.getElementById("input").value;
@@ -140,8 +197,26 @@ function jia() {
     for (let i = 0; i < list.length; i++) {
         //console.log(String(list[i]).toUpperCase())
         //console.log(replace_jia(String(list[i]).toUpperCase()))
-        list[i] = replace_jia(String(list[i]).toUpperCase());
-        result += list[i];
+        if (list[i] == list[i].toLocaleUpperCase() && isNaN(Number(list[i]))) {
+            console.log(1)
+            //console.log(replace_jia(String(list[i + 1]).toUpperCase()))
+            console.log(replace_jia(String(list[i])))
+            list[i] = "?" + replace_jia(String(list[i]));
+            result += list[i];
+            //i += 1;
+        } else {
+            if (isNaN(Number(list[i]))) {
+                list[i] = replace_jia(String(list[i]).toUpperCase()).toLowerCase();
+                result += list[i]
+            } else {
+                console.log(replace_jia(list[i]))
+                list[i] = replace_jia(list[i]);
+                result += list[i]
+            }
+
+        }
+        //list[i] = replace_jia(String(list[i]).toUpperCase());
+        //result += list[i];
     }
     console.log(result);
     document.getElementById("h2").innerHTML = result;
@@ -154,8 +229,17 @@ function jie() {
     for (let i = 0; i < list.length; i++) {
         //console.log(String(list[i]).toUpperCase())
         //console.log(replace_jia(String(list[i]).toUpperCase()))
-        list[i] = replace_jie(String(list[i]).toUpperCase());
-        result += list[i]
+        if (list[i] == "?") {
+            list[i + 1] = replace_jie(String(list[i + 1]).toUpperCase());
+            result += list[i + 1];
+            i += 1;
+            //continue
+        } else {
+            console.log(replace_jie(String(list[i])))
+            list[i] = replace_jie(String(list[i]).toUpperCase()).toLowerCase();
+            result += list[i]
+        }
+
     }
     console.log(result)
     document.getElementById("h2").innerHTML = result;
